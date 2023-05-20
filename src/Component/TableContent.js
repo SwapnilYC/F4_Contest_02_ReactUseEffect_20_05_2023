fcc1a09e4606fb5de14a8a79cb9287b5f993824c
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-05-20 19:20:14 +0530

    empty fragment is removed from table content

--- src/Component/TableContent.js
+++ src/Component/TableContent.js
@@ -3,17 +3,15 @@ import React from "react";
 function TableContent({ details }) {
   console.log('Inside tablecontent.js');
   return (
-    <>
-      <tr>
-        <td><img src={details.image} alt={details.name} /></td>
-        <td>{details.name}</td>
-        <td>{details.symbol.toUpperCase()}</td>
-        <td>{details.current_price}</td>
-        <td>{details.total_volume}</td>
-        <td>{details.market_cap}</td>
-        <td>{details.market_cap_change_percentage_24h} %</td>
-      </tr>
-    </>
+    <tr>
+      <td><img src={details.image} alt={details.name} /></td>
+      <td>{details.name}</td>
+      <td>{details.symbol.toUpperCase()}</td>
+      <td>{details.current_price}</td>
+      <td>{details.total_volume}</td>
+      <td>{details.market_cap}</td>
+      <td>{details.market_cap_change_percentage_24h} %</td>
+    </tr>
   );
 }
 
