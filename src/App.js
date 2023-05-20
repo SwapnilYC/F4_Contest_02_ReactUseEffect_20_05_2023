6b1244eddc184ac63a78605a49e0632a6108a1a9
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-05-20 20:45:13 +0530

    some changes made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import RowContent from "./Component/RowContent";
+// import axios from 'axios';
+import RowContent from "./RowContent";
+
 
 function App() {
   const [rowData, setRowData] = useState([]);
@@ -8,62 +9,43 @@ function App() {
 
   // In useEffect hook we are just calling the fetchApiData
   useEffect(() => {
-    // fetchApiData(api);
     fetch(api)
       .then(res => res.json())
       .then(data => setRowData(data))
       .then(() => { console.log(rowData) })
       .catch((error) => console.log(error))
-    console.log(rowData);
+    // console.log(rowData);
   }, [])
 
   // fetch api data is a function which will set the setRowdata  function to fetch value
-  async function fetchApiData(url) {
-    const urlData = await axios.get(url);  // will return an object 
-    setRowData(urlData.data);
+  // async function fetchApiData(url) {
+  //   const urlData = await axios.get(url);  // will return an object 
+  //   setRowData(urlData.data);
+  // setRowData(urlData);
 
-    //----------------------------------------------------------
-    // fetch url method using promise chaining
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(data => setRowData(data))
-    //   .then(() => { console.log(rowData) })
-    //   .catch((error) => console.log(error))
 
-    //----------------------------------------------------------
-    //  fetching data using async await method
-    // try {
-    //   const res = await fetch(url);
-    //   const data = await res.json();
-    //   setRowData(data);
-    //   console.log(rowData);
-    // } catch (error) {
-    //   console.log(error);
-    // }
-  }
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Symbol</th>
-            <th>Current Price</th>
-            <th>Total Volume</th>
-            <th>Total Market Cap($)</th>
-            <th>Market Cap Change In Percentage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {table rows will be append here} */}
-          {rowData.map((coinDataRow) => {
-            <RowContent key={coinDataRow.id} details={coinDataRow}></RowContent>
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Current Price</th>
+          <th>Total Volume</th>
+          <th>Total Market Cap($)</th>
+          <th>Market Cap Change In Percentage</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rowData.map(coinDataRow => {
+          // console.log(coinDataRow);
+          <RowContent key={coinDataRow.id} details={coinDataRow}></RowContent>
+        })}
+      </tbody>
+    </table>
+
   );
 }
 
